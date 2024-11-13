654a6c72dabf96210837ccbf13dab77aedcd7bab
Robert Jank <robert.jank@yahoo.com>
2024-11-13 12:06:51 -0600

    inital setup

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,19 @@
-// build your `/api/projects` router here
+const router = require("express").Router();
+const Projects = require("./model");
+
+router.get("/", (req, res, next) => {
+  try {
+    res.status(200).json({ message: "This is working" });
+  } catch (err) {
+    next(err);
+  }
+});
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: "something went wrong inside the recipes router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-// build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+const taskRouter = require("./task/router");
+const projectRouter = require("./project/router");
+const resourceRouter = require("./resource/router");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use("/api/task", taskRouter);
+server.use("/api/project", projectRouter);
+server.use("/api/resource", resourceRouter);
+
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
