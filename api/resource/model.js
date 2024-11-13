b3b57d2922fc729d780cf8df0f860bc8888239ae
Robert Jank <robert.jank@yahoo.com>
2024-11-13 13:47:15 -0600

    resource router and model done

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,24 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+async function get() {
+  const row = await db("resources");
+  return row;
+}
+
+async function getById(id) {
+  const resource = await db("resources").where("resources.resource_id", id);
+  return resource[0];
+}
+
+async function add(newResource) {
+  try {
+    const [id] = await db("resources").insert(newResource);
+    const resource = await getById(id);
+    return resource;
+  } catch (err) {
+    console.error("error adding resource", err);
+    throw new Error("could not add resource");
+  }
+}
+
+module.exports = { get, getById, add };
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,16 +1,36 @@
 const router = require("express").Router();
 const Resources = require("./model");
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    res.status(200).json({ message: "This is working" });
+    const resourcesArray = await Resources.get();
+    res.status(200).json(resourcesArray);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const resource = await Resources.getById(id);
+    res.status(200).json(resource);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const resource = await Resources.add(req.body);
+    res.status(201).json(resource);
   } catch (err) {
     next(err);
   }
 });
 router.use((err, req, res, next) => {
   res.status(500).json({
-    customMessage: "something went wrong inside the recipes router",
+    customMessage: "something went wrong inside router",
     message: err.message,
     stack: err.stack,
   });
